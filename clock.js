a5e9855559d168139cc051cbcd733eff4b98d1fa
Raul <eduardoct120@gmail.com>
2016-10-18 18:10:12 -0500

    little changes on script

---
+++ clock.js
@@ -0,0 +1,84 @@
+(function(){
+
+	var changeHour = function() {
+		var date = new Date(),
+			hours = date.getHours(),
+			ampm,
+			minutes = date.getMinutes(),
+			seconds = date.getSeconds(),
+			dayWeek = date.getDay(),
+			day = date.getDate(),
+			month = date.getMonth(),
+			year = date.getFullYear();
+
+		var pHours = document.getElementById( 'hours' ),
+			pAMPM = document.getElementById( 'ampm' ),
+			pMinutes = document.getElementById( 'minutes' ),
+			pSeconds = document.getElementById( 'segundos' ),
+			pDayWeek = document.getElementById( 'dayWeek' ),
+			pDay = document.getElementById( 'day' ),
+			pMonth = document.getElementById( 'month' ),
+			pYear = document.getElementById( 'year' );
+
+
+		var week = [ 'Sunday',
+								 'Monday',
+								 'Thuesday',
+								 'Wendsday',
+								 'Thuersday',
+								 'Friday',
+								 'Saturday' ];
+
+		pDayWeek.textContent = week[dayWeek];
+		pDay.textContent = day;
+
+		var months = [ 'January',
+									 'February',
+									 'March',
+									 'April',
+									 'May',
+									 'June',
+									 'July',
+									 'August',
+									 'September',
+									 'October',
+									 'November',
+									 'December' ];
+
+		pMonth.textContent = months[ month ];
+		pYear.textContent = year;
+
+		if ( hours >= 12 ) {
+			hours = hours - 12;
+			ampm = 'PM';
+		} else {
+			ampm = 'AM';
+		}
+
+		if( hours == 0 ){
+			hours == 12;
+		}
+
+		pHours.textContent = hours;
+		pAMPM.textContent = ampm;
+
+		if (minutes < 10) {
+			minutes = "0"+minutes;
+		};
+		
+		if (seconds <10) {
+			seconds = 0+seconds;
+		};
+
+		pMinutes.textContent = minutes;
+		pSeconds.textContent = seconds
+
+	};
+
+
+
+
+	changeHour();
+	var interval = setInterval(changeHour, 1000);
+
+}())
